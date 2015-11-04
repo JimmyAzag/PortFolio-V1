097fa734b92171d4ef7cdd68702e6a7c3dd8c830
me <me>
2015-11-04 12:47:00 +0100

    [PortFolio-V1] : Commit Final de la version 1.

---
+++ header.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <title> Porfolio AZAG Jimmy</title>
+
+        <!--[if lt IE 9]>
+        <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+
+        <!-- Fichiers CSS -->
+        <link rel="stylesheet" href="css/reset.css">
+        <!--[if lt IE 9]> 
+                <link rel="stylesheet" href="css/style.css" media="screen">
+        <![endif]-->
+        <link rel="stylesheet" media="screen and (max-width:480px)" href="css/mobile.css">
+        <link rel="stylesheet" media="screen and (min-width:481px)" href="css/style.css">
+        <link rel="stylesheet" media="print" href="css/print.css">
+        <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Cantarell">
+    </head>
+    
+    <body>
+        
+        <!-- Header -->
+        <header role="banner">
+            <div class="container_16">
+                <hgroup>
+                    <h1>AZAG Jimmy</h1>
+                    <h2>Développeur Web - PHP</h2>
+                </hgroup>
+
+                <figure>
+                    <img src="images/avatar.png" alt="AZAG Jimmy">
+                </figure>
+                
+                <div class="contact">
+                        <p><img src="images/icon-home.png" alt="mon adresse"/>44, rue du 11 novembre<br/> 
+                            42500 Le chambon Feugerolles<br/>
+                            <img src="images/icon-mail.png" alt="mon mail"/>jimmy.azag@gmail.com<br/>
+                            <img src="images/icon-phone.png" alt="mon téléphone"/>06.58.51.58.65</p>
+                </div>
+                
+                <nav>
+                    <ul>
+                        <li><a href="index.php">Accueil</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+        
+        <!-- Corps -->
+        <section role="main" class="container_16 clearfix">
---
+++ index.php
@@ -0,0 +1,108 @@
+<?php include 'header.php'; ?>
+
+<div class="grid_16">
+    <!-- A propos -->
+    <div class="grid_8 apropos">
+        <h3>A propos</h3>
+        <p>Je m’appelle AZAG Jimmy, J’ai 26 ans.</p>
+        <p>J’ai commencé à m’intéresser à l’informatique il y a 5 ans.<br/>
+            Je me suis spécialisé dans le web. La création et le maintiens en activité de sites m'a toujours plu, c'est pourquoi j'oriente mes études dans cette direction.<br/>
+        Le métier de developpeur web allie rigueur et flexibilité, des qualités que je possède.</p>
+    </div>
+
+    <!-- Compétences -->
+    <div class="grid_8 competences">
+        <h3>Compétences Techniques</h3>
+        <ul class="tags">
+            <li data-skills="100">HTML<span style="width: 100%;"></span></li>
+            <li data-skills="100">CSS<span style="width: 100%;"></span></li>
+            <li data-skills="50">PHP procedural<span style="width: 50%;"></span></li>
+            <li data-skills="50">SQL<span style="width: 50%;"></span></li>
+            <li data-skills="100">Wordpress<span style="width: 100%;"></span></li>
+            <li data-skills="100">Joomla<span style="width: 100%;"></span></li>
+        </ul>
+        <br/>
+        <h3>Notions Techniques</h3>
+        <ul class="tags">
+            <li data-skills="10">Javascript<span style="width: 10%;"></span></li>
+            <li data-skills="10">Jquery<span style="width: 10%;"></span></li>
+            <li data-skills="10">Bootstrap<span style="width: 10%;"></span></li>
+            <li data-skills="25">Prestashop<span style="width: 25%;"></span></li>
+            <li data-skills="10">Magento<span style="width: 10%;"></span></li>
+            <li data-skills="40">Photoshop<span style="width: 40%;"></span></li>
+            <li data-skills="15">Illustrator<span style="width: 15%;"></span></li>
+        </ul>
+    </div>
+</div>
+
+<!-- Expériences -->
+<div class="grid_16 experiences">
+    <h3>Expériences professionnelles</h3>
+    <div class="grid_10">
+        <ul>
+            <li>
+                <h4><strong>Webmaster (stage)</strong> chez ProGsoft</h4>
+                <span class="lieu">Voiron (38)</span>
+                <span class="dates">09/2013-06/2014</span>
+                <p>Création complète du site institutionnel <a href="http://www.progrisk.com" target="blank">www.progrisk.com</a><br/>
+                    Refonte du site institutionnel <a href="http://www.progsoft.fr" target="blank">www.progsoft.fr</a><br/>
+                   - Utilisation du CMS Wordpress<br/>
+                   - Utilisation des langages XHTML, CSS, PHP, Jquery<br/>
+                   - Utilisation du logiciel Photoshop</p>
+            </li>
+            <li>
+                <h4><strong>Webmaster (stage)</strong> chez Jardiniers de France</h4>
+                <span class="lieu">La Ricamarie (42)</span>
+                <span class="dates">01/2012-02/2012</span>
+                <p>Refonte du site institutionnel :<br/>
+                   - Utilisation du CMS JOOMLA<br/>
+                   - Utilisation des langages XHTML, CSS<br/>
+                   - Utilsation du logiciel GIMP</p>
+            </li>
+            <li>
+                <h4><strong>Webmaster (stage)</strong> chez C2M EQUIPEMENTS</h4>
+                <span class="lieu">La Fouillouse (42)</span>
+                <span class="dates">05/2011-06/2011</span>
+                <p>Création d'un site e-commerce:<br/>
+                   - Utilisation du CMS PRESTASHOP<br/>
+                   - Utilisation des langages XHTML, CSS<br/>
+                   - Utilsation du logiciel GIMP</p>
+            </li>
+        </ul>
+    </div>
+    <div class="grid_6 picto_site">
+        <img src="images/progrisk.png"/>
+        <img src="images/progsoft.png"/>
+    </div>
+</div>
+
+<!-- Formations -->
+<div class="grid_16 formations">
+    <h3>Formations</h3>
+    <ul>
+        <li>
+            <h4><strong>Développeur Web PHP/MySQL</strong> à IP Formation</h4>
+            <span class="lieu">Villeurbanne (69)</span>
+            <span class="dates">2015</span>
+        </li>
+        <li>
+            <h4><strong>Titre CRW - Concepteur Réalisateur Web</strong> à Sup2i</h4>
+            <span class="lieu">Grenoble (38)</span>
+            <span class="dates">2013-2014</span>
+        </li>
+        <li>
+            <h4><strong>BTS IG - Informatique de Gestion Opt. Developpeur d'Application</strong> à Lycée Simone Weil</h4>
+            <span class="lieu">Saint Priest-en-Jatez (42)</span>
+            <span class="dates">2010-2012</span>
+        </li>
+    </ul>
+</div>
+
+<!-- Loisirs -->
+<div class="grid_8 loisirs">
+    <h3>Loisirs</h3>
+    <p><strong>Sports :</strong> Football, Basketball</p>
+    <p><b>Autres loisirs :</b> Lecture, Jeux vidéos.</p>
+</div>
+
+<?php include 'footer.php'; ?>
